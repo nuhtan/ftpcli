81aef383f727a7fc18e182edaa80c17b9beb32e8
nuhtan <ncrichardson15@gmail.com>
2021-06-22 18:38:06 -0400

    temp main for developing ui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
-fn main() {
-    println!("Hello, world!");
+mod shared_state;
+mod ui;
+
+use std::io::Error;
+
+fn main() -> Result<(), Error> {
+    // shared_state::SharedState::new(String::from("nuhtan"), String::from("test"), [192, 168, 1, 7].into(), 22);
+    ui::start_up();
+    Ok(())
 }
